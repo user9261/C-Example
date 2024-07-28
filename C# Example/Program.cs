8f3d73f7193a7e0806e9efebeae82e8b594d0ad2
user9261 <nre12270@gmail.com>
2024-07-28 17:33:38 +0900

    プロジェクト ファイルを追加します。

---
+++ C# Example/Program.cs
@@ -0,0 +1,10 @@
+﻿// 変数の宣言
+int money;     //所持金を代入する変数
+string name;   //名前を入力する変数
+
+//変数に値を代入する
+money = 5000;
+name = "きたむら";
+
+Console.WriteLine(money); //所持金を表示
+Console.WriteLine(name);　//名前を表示
